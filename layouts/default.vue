6ba7fb36bd707a6c65dc327c7e2efee9ee68cf12
Hye-Kyung9 <rngprud123@naver.com>
2024-06-27 14:18:30 +0900

    feat: NuxtLink적용 / layout생성

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">HK NUXT DOJO</NuxtLink>
+        <ul>
+          <li>
+            <NuxtLink to="/">HOME</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">ABOUT</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/products">PRODUCTS</NuxtLink>
+          </li>
+          <a href="/about">normal link- about page</a>
+        </ul>
+      </nav>
+    </header>
+
+    <div>
+      <slot />
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active{
+  color: #12b488;
+}
+</style>
---
+++ layouts/products.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/products">HK NUXT DOJO</NuxtLink>
+      </nav>
+    </header>
+
+    <div>
+      <slot />
+    </div>
+
+    <footer>
+      <ul>
+        <li>
+          <NuxtLink to="/">HOME</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">ABOUT</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/products">PRODUCTS</NuxtLink>
+        </li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
