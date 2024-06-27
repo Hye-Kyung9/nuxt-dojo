339c2db8a17b88cbd5b2e7260c8dd6a9b9dcbcb9
Hye-Kyung9 <rngprud123@naver.com>
2024-06-27 13:54:00 +0900

    feat: 폴더 라우팅 설정 다이나믹 라우팅 생성

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <p>Hello, HK</p>
-  </div>
-</template>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem Ipsem 1</p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
