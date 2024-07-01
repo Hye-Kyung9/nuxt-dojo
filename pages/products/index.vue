43a63c0a98422b340339832499fd6859c0b4229b
Hye-Kyung9 <rngprud123@naver.com>
2024-07-01 10:05:51 +0900

    feat: product 컴포넌트화

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,20 +2,18 @@
   <div>
     <div class="grid grid-cols-4 gap-5">
       <div v-for="p in products">
-        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+        <ProductCard :product="p"/>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import ProductCard from '~/components/ProductCard.vue';
+
   definePageMeta({
     layout: 'products'
   })
 
 const {data : products} = await useFetch('https://fakestoreapi.com/products')
 </script>
-
-<style lang="scss" scoped>
-
-</style>
